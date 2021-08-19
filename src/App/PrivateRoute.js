18dddff53280fb8407772fe0c67b2a9096405bef
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-08-19 09:10:25 +0300

    reset check

--- src/App/PrivateRoute.js
+++ src/App/PrivateRoute.js
@@ -4,7 +4,7 @@ import { Redirect, Route } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
     // Add your own authentication on the below line.
-    const isLoggedIn = true;
+    const isLoggedIn = rest.isLoggedIn;
 
     return (
         <Route
